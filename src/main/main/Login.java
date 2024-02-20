da3c3fcf6c17067e4857f0b52abe4d696aacd4c4
kritikashrmaa <ks916527@gmail.com>
2024-02-20 22:35:40 +0530

    Add swing screen

---
+++ src/main/main/Login.java
@@ -0,0 +1,24 @@
+package src.main.main;
+
+import javax.swing.*;
+
+public class Login extends JFrame {
+
+
+    Login(){
+
+        setTitle("ATM");
+
+        // defining the length and breadth of the frame
+        setSize(900,400);
+
+        // setting the visibility as true
+        setVisible(true);
+
+    }
+
+    public static void main(String args[]){
+
+        new Login();
+    }
+}
